addc366a028c19b9b9394bdbb76944c65e4207a7
0ut0fc0d3 <elopjim829@g.educaand.es>
2023-03-17 17:54:12 +0100

    Acabado top level window management

--- src/jvmMain/kotlin/Getting_Started/Getting_Started.kt
+++ src/jvmMain/kotlin/Getting_Started/Getting_Started.kt
@@ -1,3 +1,5 @@
+package Getting_Started
+
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
